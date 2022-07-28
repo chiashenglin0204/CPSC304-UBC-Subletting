f9bb7682b89b7cbf116063b2b9cc2d093604d4eb
kenny.cheng <kenny.cheng@unity3d.com>
2022-07-28 03:13:57 -0700

    E2E working, can see react FE successful response from network tab in browser, display not working however, need to look into proper react api calling

--- react-ui/README.md
+++ react-ui/README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# components library
+[Material UI](https://mui.com/material-ui/getting-started/overview/)
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 Run development mode on `localhost:3000` with hot reload
@@ -11,9 +11,14 @@ launch test runner in interactive watch mode. [more info](https://facebook.githu
 Build for production into `build` folder. [more info](https://facebook.github.io/create-react-app/docs/deployment)
 ### `npm run build`
 
-## Learn More
+
+## --------- ADDITIONAL READING --------------
+### Learn More
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
 
+### Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import Typography from '@mui/material/Typography';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [dbData, setDbData] = useState('')
+
+  useEffect(() => {
+    fetch("http://localhost:3001/users", { mode: "no-cors" })
+            .then(res => {
+              console.log(res);
+              res.json();
+            });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant="body1">
+          {dbData}
+        </Typography>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
