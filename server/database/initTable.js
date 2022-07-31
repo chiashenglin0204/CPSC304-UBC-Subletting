10c65c22db19472d3b724cdea3d450cba3b9f275
chiashenglin <bryan2160088@gmail.com>
2022-07-31 02:21:27 -0700

    initTable

---
+++ server/database/initTable.js
@@ -0,0 +1,43 @@
+var connection = require('sequelize');
+const initTables = async () => {
+	await initUsers();
+    await initSubletters();
+};
+
+const initUsers = async () => {
+	await connection
+		.query(
+			`
+                CREATE TABLE IF NOT EXISTS "users" (
+                sid     Integer   PRIMARY KEY,
+                "phone#"  TEXT      NOT NULL,
+                name    TEXT      NOT NULL,
+                gender  CHAR(1)   NOT NULL,
+                email   TEXT,
+                UNIQUE ("phone#")
+                );
+            `,
+			{ type: connection.QueryTypes.CREATE }
+		)
+		.then(() => console.log('successfully created users table'))
+		.catch(e => console.error(e.stack));
+};
+
+const initSubletters = async () => {
+	await connection
+		.query(
+			`
+                CREATE TABLE IF NOT EXISTS Subletter (
+                subID   Serial,
+                sid     Integer,
+                PRIMARY KEY (subID, sid),
+                FOREIGN KEY (sid) REFERENCES "user" (sid)
+                );
+            `,
+			{ type: connection.QueryTypes.CREATE }
+		)
+		.then(() => console.log('successfully created Subletters table'))
+		.catch(e => console.error(e.stack));
+};
+
+module.exports = initTables;
