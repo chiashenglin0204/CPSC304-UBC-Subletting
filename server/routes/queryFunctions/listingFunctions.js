5b2b32885d95ccb0a4e21c87f00114d00b05660f
kenny.cheng <kenny.cheng@unity3d.com>
2022-07-31 19:39:55 -0700

    made nested aggregation with group by query

--- server/routes/queryFunctions/listingFunctions.js
+++ server/routes/queryFunctions/listingFunctions.js
@@ -1,14 +1,49 @@
-const { readFileSync } = require('fs');
 const connection = require('../../database/sequelize.js');
 
 module.exports.getListingCountByRoomType = async (req, res) => {
-	const query = readFileSync('./database/aggregation_groupby.sql', 'utf8');
+	const query = `
+        SELECT COUNT(l.listingID) as numListings, r.roomType
+        FROM listing as l
+        INNER JOIN room_in12 as r 
+            ON l.resID=r.resID AND l."room#"=r."room#"
+        GROUP BY r.roomType
+        ORDER BY r.roomType;
+    `;
 	try {
 		const queryRes = await connection.query(query, { type: connection.QueryTypes.SELECT });
 		return res.json(queryRes);
 	} catch (e) {
 		console.error(e);
-		return res.status(404).json(e);
+		return res.status(404).json({ error: e });
 	}
 }
 
+/**
+ * @param req.body.status REQUIRED
+ */
+module.exports.getMinPriceListings = async (req, res) => {
+    if (req.body.status === null)
+    return res.status(400).send('missing required params');
+    
+    try {
+        const query = `
+            SELECT *
+            FROM listing l
+            WHERE rate = (
+                SELECT MIN(l2.rate)
+                FROM listing l2
+            ) AND status=?;
+        `;
+        const queryRes = await connection.query(query, {
+            type: connection.QueryTypes.SELECT,
+            replacements: [
+                req.body.status
+            ]
+        });
+
+        return res.json(queryRes);
+    } catch (e) {
+        console.error(e);
+        return res.status(404).json({ error: e });
+    }
+}
