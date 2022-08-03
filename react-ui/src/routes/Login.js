b481dd5a8d1d07d97acc8a9cbc1bbc1cd4057210
kenny.cheng <kenny.cheng@unity3d.com>
2022-08-02 17:11:38 -0700

    setting up stuff

---
+++ react-ui/src/routes/Login.js
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div className="App">
+      <form className="form">
+        <Button>Hello</Button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
