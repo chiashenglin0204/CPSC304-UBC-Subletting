504002398c388016ba0def1c022b4d10fcc35e87
chiashenglin <bryan2160088@gmail.com>
2022-08-02 21:59:25 -0700

    merge change with main

--- server/routes/application.js
+++ server/routes/application.js
@@ -1,28 +1,13 @@
 var express = require('express');
 var router = express.Router();
-// var createUser = require('./route-helpers/users-helpers');
 var {
 	deleteApplicationById,
 	getApplicationBySid,
 	createApplication,
 } = require('./route-helpers/application-helpers');
-/* GET users listing. */
-// router.get('/users', function (req, res, next) {
-//   const query = 'select * from users;'
-//   console.log(`running ${query}`);
-//   connection.query(query, { type: connection.QueryTypes.SELECT })
-//     .then(users => {
-//       console.log("ran query");
-//       console.log(users)
-//       res.json(users)
-//     })
-//     .catch (error => console.error(error))
-// })
 
-//router.post('/createApplication', createApplication);
 router.delete('/deleteApplicationById', deleteApplicationById);
 router.get('/getApplicationBySid', getApplicationBySid);
 router.post('/createApplication', createApplication);
-
 
 module.exports = router;
--- server/routes/subletter.js
+++ server/routes/subletter.js
@@ -1,21 +1,7 @@
 var express = require('express');
 var router = express.Router();
-// var createUser = require('./route-helpers/users-helpers');
 var { selectApplicationOrListingByName } = require('./route-helpers/subletter-helpers');
-/* GET users listing. */
-// router.get('/users', function (req, res, next) {
-//   const query = 'select * from users;'
-//   console.log(`running ${query}`);
-//   connection.query(query, { type: connection.QueryTypes.SELECT })
-//     .then(users => {
-//       console.log("ran query");
-//       console.log(users)
-//       res.json(users)
-//     })
-//     .catch (error => console.error(error))
-// })
 
-//router.post('/createApplication', createApplication);
 router.get(
 	'/selectApplicationOrListingByName',
 	selectApplicationOrListingByName
