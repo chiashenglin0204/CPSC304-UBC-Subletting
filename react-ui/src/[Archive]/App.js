b5cf7eb905c59d1465453884c55e628ec896f0fd
kenny.cheng <kenny.cheng@unity3d.com>
2022-08-03 17:35:16 -0700

    formatted with prettier

--- react-ui/src/[Archive]/App.js
+++ react-ui/src/[Archive]/App.js
@@ -1,36 +1,36 @@
-import './App.css'
-import Typography from '@mui/material/Typography'
-import { useEffect, useState } from 'react'
+import './App.css';
+import Typography from '@mui/material/Typography';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [dbData, setDbData] = useState()
+	const [dbData, setDbData] = useState();
 
-  const fetchUsersJSON = async () => {
-    const response = await fetch('http://localhost:3001/users/')
-    if (!response.ok) {
-      const message = `An error has occured: ${response.status}`
-      throw new Error(message)
-    }
+	const fetchUsersJSON = async () => {
+		const response = await fetch('http://localhost:3001/users/');
+		if (!response.ok) {
+			const message = `An error has occured: ${response.status}`;
+			throw new Error(message);
+		}
 
-    const users = await response.json()
-    console.log(users)
-    return users
-  }
+		const users = await response.json();
+		console.log(users);
+		return users;
+	};
 
-  useEffect(() => {
-    fetchUsersJSON().then((res) => setDbData(res))
-  }, [])
+	useEffect(() => {
+		fetchUsersJSON().then((res) => setDbData(res));
+	}, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Typography variant="body1">{String(dbData)}</Typography>
-      </header>
-    </div>
-  )
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<Typography variant="body1">{String(dbData)}</Typography>
+			</header>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- react-ui/src/routes/Login.js
+++ react-ui/src/routes/Login.js
@@ -1,23 +1,37 @@
-import { Button, TextField, Typography } from '@mui/material'
-import React from 'react'
-import './Login.css'
+import { Button, TextField, Typography } from '@mui/material';
+import React from 'react';
+import './Login.css';
 
 const Login = () => {
+	return (
+		<div>
+			<form className="form">
+				<Typography variant="h2">Welcome to UBC Subletting!</Typography>
+				<Typography variant="subtitle1">
+					please login with your student ID and phone number as your
+					password to continue
+				</Typography>
+				<Typography variant="subtitle2">
+					(input phone number with no formatting e.g. 123456789)
+				</Typography>
+				<TextField
+					id="outlined-basic"
+					label="Student ID"
+					variant="outlined"
+				/>
+				<TextField
+					id="outlined-basic"
+					label="Phone Number"
+					variant="outlined"
+					type="password"
+				/>
+				<Button variant="contained">Login</Button>
+				<Button LinkComponent={'a'} variant="contained" href="/Signup">
+					Sign up
+				</Button>
+			</form>
+		</div>
+	);
+};
 
-
-  return (
-    <div>
-      <form className="form">
-        <Typography variant="h2">Welcome to UBC Subletting!</Typography>
-        <Typography variant="subtitle1">please login with your student ID and phone number as your password to continue</Typography>
-        <Typography variant="subtitle2">(input phone number with no formatting e.g. 123456789)</Typography>
-        <TextField id="outlined-basic" label="Student ID" variant="outlined" />
-        <TextField id="outlined-basic" label="Phone Number" variant="outlined" type="password" />
-        <Button variant="contained" >Login</Button>
-        <Button LinkComponent={'a'} variant="contained" href='/Signup'>Sign up</Button>
-      </form>
-    </div>
-  )
-}
-
-export default Login
+export default Login;
