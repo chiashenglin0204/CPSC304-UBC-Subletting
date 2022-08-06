337b62aed74023d7133f408394b035a3daad9e25
kenny.cheng <kenny.cheng@unity3d.com>
2022-08-05 20:27:26 -0700

    added docs for how to use useUserContext hook

--- react-ui/src/components/UserContext.js
+++ react-ui/src/components/UserContext.js
@@ -13,6 +13,19 @@ UserContextProvider.propTypes = {
   children: PropTypes.node,
 };
 
+/**
+ * 
+ * @returns { object, function } returns an object containing the currently logged in user's SID and Phone#
+ * How to use:
+ *    const { user, setUser } = useUserContext()
+ * now you can use the user variable to access the user info
+ * setUser can be used to set new values by inputting an object containing sid and phone#
+ * e.g.
+ *    setUser({
+ *      ...user,        // <- this line uses the spread operator which inputs all the user's old properties, it's unnecessary if you are changing both sid and phone#
+ *      sid: '420',
+ *    })
+ */
 const useUserCountext = () => {
   const context = React.useContext(UserContext);
   if (context === undefined) {
