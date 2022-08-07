a52758cbaf22386cc8e173e8513b80627b758d4a
kenny.cheng <kenny.cheng@unity3d.com>
2022-08-07 01:32:26 -0700

    created FE query for minpricelistingbyroomtype

--- react-ui/src/components/ApplicantContent.js
+++ react-ui/src/components/ApplicantContent.js
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import {
   getAllListings,
--- react-ui/src/requests/listingRequests.js
+++ react-ui/src/requests/listingRequests.js
@@ -12,3 +12,17 @@ export const getListingCountForRoomTypes = () =>
  */
 export const getAllListings = () =>
   fetchDbData('GET', 'http://localhost:3001/listing/getAll');
+
+/**
+ *
+ * @param {string} searchParams URL search params created from new URLSearchParams({ sid: 0, phoneNum: '0' })
+ *                              roomType REQUIRED
+ * @returns {Object} json object with array of listing or error
+ *  []{id, datelisted, status, rate, startdate, enddate, roomtype, gender, haskitchen, numRooms, numBathrooms}
+ */
+export const getCheapestAvailableListingByRoomType = (searchParams) => {
+  fetchDbData(
+    'GET',
+    'http://localhost:3001/listing/minPriceListingByRoomType/?' + searchParams
+  );
+};
